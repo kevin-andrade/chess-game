38d98f02de5769f561698c2e3c4e3e4f9d596bc0
Keven Andrade <nivekandrade@live.com>
2023-10-19 15:01:49 -0300

    Creating a custom board exception

--- chess-console/entities/board/Board.cs
+++ chess-console/entities/board/Board.cs
@@ -14,9 +14,31 @@ namespace board {
             return Pieces[line, column];
         }
 
+        public Piece Piece(Position pos) {
+            return Pieces[pos.Line, pos.Column];
+        }
+
+        public bool PieceExist(Position pos) {
+            ValidatePosition(pos);
+            return Piece(pos) != null;
+        }
+
         public void AddPart(Piece p, Position pos) {
             Pieces[pos.Line, pos.Column] = p;
             p.Position = pos;
+        }
+
+        public bool ValidPosition(Position pos) {
+            if (pos.Line <0 || pos.Line >= Lines || pos.Column <0 || pos.Column >= Columns) {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position pos) {
+            if (!ValidPosition(pos)) {
+                throw new BoardException("Invalid position!");
+            }
         }
     }
 }
